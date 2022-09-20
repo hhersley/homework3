4a55068dc164e834549d4cf68611f5e330e6fbbe
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-20 17:40:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Homework2</title>
+    <title>Homework3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
+<body>
+  <h1> Homework3 </h1>
+  </body>
