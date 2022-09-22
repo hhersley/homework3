3614a584baaca2b09ba60e457bd1a686f172fac4
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-22 12:34:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,52 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Homework3</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
-<body>
-  <h1> Homework3 </h1>
-  </body>
+  <body>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Series ID</th>
+      <th>Name</th>
+      <th>Main Character</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "hahersle_homework3	";
+$password = "V8N9M37o{2Q2";
+$dbname = "hahersle_homework3	";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT series_id, series_name, main_character from Series";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["series_id"]?></td>
+    <td><?=$row["series_name"]?></td>
+    <td><?=$row["main_character"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+      </tbody>
+    </table>
+    <h1>Hello, world!</h1>
+    
+    
