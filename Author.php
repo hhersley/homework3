f59eb87a9c79308f4c0dddf0bd55d31bd2942455
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-26 22:09:46 -0500

    Create Author.php

---
+++ Author.php
@@ -0,0 +1,48 @@
+<?php require_once('header.php'); ?>
+
+    
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Author ID</th>
+      <th>Name</th>
+      <th>Series</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "hahersle_homework3";
+$password = "Hello10513!!";
+$dbname = "hahersle_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT A.author_id, author_name, series_name FROM Author A Join Series S on A.author_id = S.author_id";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["A.author_id"]?></td>
+    <td><?=$row["author_name"]?></td>
+    <td><?=$row["series_name"]?></td>
+
+
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+      </tbody>
+    </table>
