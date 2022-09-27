e00847cd3d7549baa7a8f39e5691b4c8dea3ea67
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-27 12:01:30 -0500

    Update Book.php

--- Book.php
+++ Book.php
@@ -23,7 +23,7 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-     <div id="card" class="card"  style="width: 18rem;">
+     <div id="card" class="card"  style="width: 15rem;">
   <img  src=<?=$row["image"]?> class="card-img-top" alt="...">
   <div  class="card-body">
     <h5 class="card-title"><?=$row["title"]?></h5>
