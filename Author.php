15df049de544d2073a6e6e90155e772cf95e652f
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-27 12:26:36 -0500

    Update Author.php

--- Author.php
+++ Author.php
@@ -1,16 +1,9 @@
+
+
 <?php require_once('header.php'); ?>
 
-    
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>Author ID</th>
-      <th>Name</th>
-      <th>Series</th>
-        <th>Book title</th>
-    </tr>
-  </thead>
-  <tbody>
+  <body>
+<div class="card-group">
     <?php
 $servername = "localhost";
 $username = "hahersle_homework3";
@@ -29,17 +22,23 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
-  while($row = $result->fetch_assoc()) {
+ ?>
+   <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?=$row["author_name"]?></h5>
+      <p class="card-text"><ul>
+<?php
+    $section_sql = "SELECT DISTINCT A.author_id, author_name, series_name, title FROM Author A Join Series S on A.author_id = S.author_id Join Book B on S.series_id = B.series_id where S.series_id=" . $row["series_id"];
+    $section_result = $conn->query($section_sql);
+    
+    while($section_row = $section_result->fetch_assoc()) {
+      echo "<li>" . $section_row["series_name"] . "</li>";
+       
+    }
 ?>
-  <tr>
-    <td><?=$row["author_id"]?></td>
-    <td><?=$row["author_name"]?></td>
-    <td><?=$row["series_name"]?></td>
-         <td><?=$row["title"]?></td>
-
-
-
-  </tr>
+      </ul></p>
+  </div>
+    </div>
 <?php
   }
 } else {
