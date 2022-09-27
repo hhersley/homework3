b8dc2143cfc14fcd575eafd87a0ed1a1c5154dee
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-27 12:12:26 -0500

    Update Series-Book.php

--- Series-Book.php
+++ Series-Book.php
@@ -24,9 +24,7 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-     <h1>Books in <?php . $sid ?></h1>
-  
-   <div id="card" class="card"  style="width: 15rem; height: 30rem;">
+     <div id="card" class="card"  style="width: 15rem; height: 30rem;">
   <img  src=<?=$row["image"]?> class="card-img-top" alt="...">
   <div  class="card-body">
     <h5 class="card-title"><?=$row["title"]?></h5>
@@ -41,8 +39,8 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-      </card-group>
+  </card-group>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
-    
+</html>
