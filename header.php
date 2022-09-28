aa4888b7392ae185d8091ab9c2ef1bee0754a4c0
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-28 13:39:05 -0500

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
     <title>Homework3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
      <link rel="stylesheet" href="StyleSheet2.css" />
-         <link rel="stylesheet" href="StyleSheet2.css" />
 
   </head>
   <body>
