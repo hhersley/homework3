c532013766e68a46a1668b31f6be3596160d40bd
hhersley <89473870+hhersley@users.noreply.github.com>
2022-09-28 20:02:50 -0500

    Update HandlePost.php

--- HandlePost.php
+++ HandlePost.php
@@ -40,13 +40,15 @@ if ($result->num_rows > 0) {
     </div>
   </div>
     </div>
+   <p>
 <?php
   }
 } else {
-  echo "  So sorry, that isn't in our library!";
+  echo "So sorry, that isn't in our library!";
 }
 $conn->close();
 ?>
+   </p>
   </card-group>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
